bcccda6be94de2aaa8a3049f1f0507862674e7e0
Suzi1i1i <105808052+Suzi1i1i@users.noreply.github.com>
2022-07-14 17:58:38 -0600

    Add files via upload

--- Home.js
+++ Home.js
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import Icons from './Icons.js';
 import Bar  from './Bar.js';
 
-const Home = () => {
+const Home = (props) => {
   return (
     <View>
-      <Bar />
+      <Bar loggedInUser={props.loggedInUser}/>
       <Icons />
     </View>
   );
